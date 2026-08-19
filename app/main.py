344dfa05cd6d25e270998f18b84bd58be8da2a6d
yuniochan-tt <ib250015yen@setouchicollge.com>
2026-08-19 13:04:08 +0900

    bai5: connect frontend

--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ app = FastAPI(title=settings.app_title)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        # TODO 
+        "http://localhost:5173",
+        "http://127.0.0.1:5173", 
     ],
     allow_credentials=True,
     allow_methods=["*"],
